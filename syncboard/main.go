02a7283e05c94bbf86c3feca81c14866f824ff13
mofon <immofon@163.com>
2020-08-26 13:26:54 +0800

    enable TLS

--- syncboard/main.go
+++ syncboard/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 	"tmp/syncmap"
 
@@ -15,17 +16,79 @@ import (
 
 var LongPollHoldTime = time.Minute * 3
 var CheckVersionInteval = time.Second / 2
+var DefaultOpListSize = 50
+
+var TLS_KEY = Getenv("TLS_KEY")
+var TLS_CERT = Getenv("TLS_CERT")
+
+func Getenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		fmt.Println("Expect set env ", k)
+	}
+	return v
+}
+
+type SyncMaps struct {
+	mu   *sync.Mutex
+	maps map[string]*syncmap.SyncMap
+}
+
+func NewSyncMaps() *SyncMaps {
+	return &SyncMaps{
+		mu:   new(sync.Mutex),
+		maps: make(map[string]*syncmap.SyncMap),
+	}
+}
+
+func (sms *SyncMaps) Get(name string) *syncmap.SyncMap {
+	sms.mu.Lock()
+	defer sms.mu.Unlock()
+
+	return sms.maps[name]
+}
+
+func (sms *SyncMaps) Create(name string) *syncmap.SyncMap {
+	sms.mu.Lock()
+	defer sms.mu.Unlock()
+
+	m := sms.maps[name]
+	if m == nil {
+		m = syncmap.New(DefaultOpListSize)
+		sms.maps[name] = m
+		m.Del(name)
+		m.ForceAchieve()
+	}
+	return m
+}
+func (sms *SyncMaps) GC() {
+	sms.mu.Lock()
+	mapclone := make(map[string]*syncmap.SyncMap)
+	for name, m := range sms.maps {
+		mapclone[name] = m
+	}
+	sms.mu.Unlock()
+
+	for name, m := range mapclone {
+		if m.Size() == 0 {
+			sms.mu.Lock()
+			delete(sms.maps, name)
+			sms.mu.Unlock()
+		}
+	}
+}
 
 func main() {
 	r := mux.NewRouter()
 
-	sm := syncmap.New(100)
-	expectname := os.Getenv("name")
-	if expectname == "" {
-		expectname = "test"
-	}
-	sm.Del(expectname)
-	sm.ForceAchieve()
+	syncms := NewSyncMaps()
+
+	go func() {
+		for {
+			time.Sleep(time.Hour)
+			syncms.GC()
+		}
+	}()
 
 	r.HandleFunc("/sync/map/{name}/{version:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
@@ -36,21 +99,25 @@ func main() {
 		}
 		fmt.Println(name)
 
-		if name == expectname {
-			for i := 0; i < int(LongPollHoldTime/CheckVersionInteval); i++ {
-				if sm.Version() != version {
-					break
-				}
-				time.Sleep(CheckVersionInteval)
+		var sm *syncmap.SyncMap
+		for i := 0; i < int(LongPollHoldTime/CheckVersionInteval); i++ {
+			if sm == nil {
+				sm = syncms.Get(name)
 			}
+			if sm != nil && sm.Version() != version {
+				break
+			}
+			time.Sleep(CheckVersionInteval)
+		}
 
-			patch := sm.Diff(version)
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(patch)
-		} else {
+		if sm == nil {
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
+
+		patch := sm.Diff(version)
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(patch)
 	}).Methods(http.MethodGet)
 
 	r.HandleFunc("/sync/map/{name}/{key}", func(w http.ResponseWriter, r *http.Request) {
@@ -58,20 +125,14 @@ func main() {
 		name := vars["name"]
 		key := vars["key"]
 
-		if name == expectname {
-			data, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			sm.Set(key, string(data))
-			w.WriteHeader(http.StatusOK)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
+		syncms.Create(name).Set(key, string(data))
+		w.WriteHeader(http.StatusOK)
 	}).Methods(http.MethodPost)
 
 	r.HandleFunc("/sync/map/{name}/{key}", func(w http.ResponseWriter, r *http.Request) {
@@ -79,7 +140,8 @@ func main() {
 		name := vars["name"]
 		key := vars["key"]
 
-		if name == expectname {
+		sm := syncms.Get(name)
+		if sm != nil {
 			sm.Del(key)
 			w.WriteHeader(http.StatusOK)
 		} else {
@@ -96,5 +158,8 @@ func main() {
 		})
 	})
 
-	http.ListenAndServe(":3669", r)
+	err := http.ListenAndServeTLS(":3669", TLS_CERT, TLS_KEY, r)
+	if err != nil {
+		panic(err)
+	}
 }
